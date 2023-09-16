5a5f6dcf4c3ca33ed226d44fdb222a33714ac3d7
petar-stojanovic <2001petarstojanovic@gmail.com>
2023-09-16 11:36:07 +0200

    changed file names. added viosualization by category on linechart

--- frontend/src/components/ChartsImpl/LineChartImpl.jsx
+++ frontend/src/components/ChartsImpl/LineChartImpl.jsx
@@ -10,6 +10,11 @@ import {
 } from "chart.js";
 import React, { useEffect, useState } from "react";
 
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+
 import { Line } from "react-chartjs-2";
 
 // import { enUS } from "date-fns/locale";
@@ -33,6 +38,7 @@ export const LineChartImpl = ({
   colors,
 }) => {
   const [chart, setChart] = useState([]);
+  const [dataShown, setDataShown] = useState("Element");
 
   useEffect(() => {
     setChart(dataset.data);
@@ -43,39 +49,42 @@ export const LineChartImpl = ({
   const items = chart?.map((row) => row["Item"]).slice(0, size);
   const elements = chart?.map((row) => row["Element"]).slice(0, size);
 
+  const elementsToShow = [
+    ...new Set(chart?.map((row) => row[`${dataShown}`]).slice(0, size)),
+  ];
+  // console.log(elementsToShow);
+ 
+  const datasets = elementsToShow.map((element,i) => {
+    const datasetValues = values.filter((value, index) => {
+      // Filter values based on the element and size
+      const dataElement = chart[index][dataShown];
+      return dataElement === element && index < size;
+    });
+  
+    const dataset = {
+      label: `${valueKey} (${element})`,
+      data: datasetValues,
+      backgroundColor: colors[i],
+      borderColor: "rgba(247, 153, 166, 1)",
+      borderWidth: 1.25,
+    };
+  
+    return dataset;
+  });
+  
+  // console.log(datasets, elementsToShow)
+
   const xScaleType =
     labelKey.toLowerCase().trim() === "year" ? "linear" : "category";
-  const yScaleType =
-    valueKey.toLowerCase().trim() === "year" ? "linear" : "category";
 
-  console.log(xScaleType, yScaleType);
-  console.log(elements);
+  const handleDataShown = (event) => {
+    setDataShown(event.target.value);
+  };
 
   var data = {
     //x-axis
-    // time or categories
     labels: labels,
-    // labels: [...new Set(labels)],
-    datasets: [
-      {
-        label: `${valueKey}`,
-      //   labels: [
-      //     'green',
-      //     'yellow',
-      //     'red',
-      //     'purple',
-      //     'blue',
-      // ],
-        //y-axis
-        data: values,
-        // data: [...new Set(values)],
-
-        backgroundColor: colors,
-        // borderColor: colors,
-        borderColor: "rgba(247, 153, 166, 1)",
-        borderWidth: 1.25,
-      },
-    ],
+    datasets: datasets
   };
 
   var options = {
@@ -131,8 +140,27 @@ export const LineChartImpl = ({
   };
 
   return (
-    <div>
-      <Line data={data} height={400} options={options} />
-    </div>
+    <>
+      <FormControl sx={{ m: 1, minWidth: 160 }}>
+        <InputLabel htmlFor="label-select">Label</InputLabel>
+        <Select
+          value={dataShown}
+          onChange={handleDataShown}
+          id="label-select"
+          label="Label"
+        >
+          {dataset?.attributes
+            .filter((label) => label !== labelKey && label !== valueKey)
+            .map((attribute, index) => (
+              <MenuItem key={index} value={attribute}>
+                {attribute}
+              </MenuItem>
+            ))}
+        </Select>
+      </FormControl>
+      <div>
+        <Line data={data} height={400} options={options} />
+      </div>
+    </>
   );
 };

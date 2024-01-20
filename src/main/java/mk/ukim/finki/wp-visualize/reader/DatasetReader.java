b413d2e0acbf21498868737dccbe4b40dc109f3d
petar-stojanovic <2001petarstojanovic@gmail.com>
2024-01-20 19:25:23 +0100

    trying to fix for heroku

--- src/main/java/mk/ukim/finki/wp-visualize/reader/DatasetReader.java
+++ src/main/java/mk/ukim/finki/wp-visualize/reader/DatasetReader.java
@@ -8,69 +8,60 @@ import mk.ukim.finki.wpvisualize.domain.Dataset;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class DatasetReader {
     private final ObjectMapper objectMapper;
+    private final PathMatchingResourcePatternResolver resolver;
 
     public DatasetReader(ObjectMapper objectMapper) {
         this.objectMapper = objectMapper;
+        this.resolver = new PathMatchingResourcePatternResolver();
     }
 
     public List<Dataset> getAllDatasets() throws IOException {
-//        List<Dataset> datasets = new ArrayList<>();
-//        File directory = new File("src/main/resources/datasetsCSV/");
         List<Dataset> datasets = new ArrayList<>();
-        Resource resource = new ClassPathResource("datasetsCSV/");
-        File directory = resource.getFile();
+        // Search for all CSV files in the datasetsCSV directory
+        Resource[] resources = resolver.getResources("classpath:datasetsCSV/*.csv");
 
-        if (directory.isDirectory()) {
-            File[] files = directory.listFiles((dir, name) -> name.endsWith(".csv"));
-
-            if (files != null) {
-                for (File file : files) {
-                    Dataset dataset = readFromFile(file);
-                    datasets.add(dataset);
-                }
-            }
+        for (Resource resource : resources) {
+            Dataset dataset = readFromResource(resource);
+            datasets.add(dataset);
         }
 
         return datasets;
     }
 
-    private Dataset readFromFile(File file) throws IOException {
-        String name = file.getName()
-                .replace("_", " ")
-                .replace(".csv", "");
-        String filePath = file.getPath();
+    private Dataset readFromResource(Resource resource) throws IOException {
+        String name = resource.getFilename()
+                      .replace("_", " ")
+                      .replace(".csv", "");
+        String filePath = resource.getURL().getPath();
 
         Dataset dataset = new Dataset();
         dataset.setName(name);
-        dataset.setFilePath(filePath);
 
         List<JsonNode> dataList = new ArrayList<>();
         List<String> attributes = new ArrayList<>();
 
-        try (CSVParser csvParser = new CSVParser(new FileReader(filePath), CSVFormat.DEFAULT)) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(resource.getInputStream()));
+             CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT.withFirstRecordAsHeader())) {
             for (CSVRecord record : csvParser) {
-                if (attributes.isEmpty()) {
-                    record.forEach(attributes::add);
-                } else {
-                    ObjectNode dataNode = objectMapper.createObjectNode();
-                    for (int i = 0; i < record.size(); i++) {
-                        dataNode.put(attributes.get(i), record.get(i));
-                    }
-                    dataList.add(dataNode);
+                ObjectNode dataNode = objectMapper.createObjectNode();
+                for (String attribute : csvParser.getHeaderMap().keySet()) {
+                    dataNode.put(attribute, record.get(attribute));
                 }
+                dataList.add(dataNode);
             }
         }
 
@@ -78,5 +69,30 @@ public class DatasetReader {
         dataset.setFileData(dataJsonArray);
 
         return dataset;
+//        Dataset dataset = new Dataset();
+//        dataset.setName(name);
+//        dataset.setFilePath(filePath);
+//
+//        List<JsonNode> dataList = new ArrayList<>();
+//        List<String> attributes = new ArrayList<>();
+//
+//        try (CSVParser csvParser = new CSVParser(new FileReader(filePath), CSVFormat.DEFAULT)) {
+//            for (CSVRecord record : csvParser) {
+//                if (attributes.isEmpty()) {
+//                    record.forEach(attributes::add);
+//                } else {
+//                    ObjectNode dataNode = objectMapper.createObjectNode();
+//                    for (int i = 0; i < record.size(); i++) {
+//                        dataNode.put(attributes.get(i), record.get(i));
+//                    }
+//                    dataList.add(dataNode);
+//                }
+//            }
+//        }
+//
+//        ArrayNode dataJsonArray = objectMapper.valueToTree(dataList);
+//        dataset.setFileData(dataJsonArray);
+//
+//        return dataset;
     }
 }

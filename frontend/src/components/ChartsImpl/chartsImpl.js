6e6c437d6075026cfb8606d9618b854566b6db7d
petar-stojanovic <2001petarstojanovic@gmail.com>
2024-01-20 18:35:22 +0100

    removed front-end

--- frontend/src/components/ChartsImpl/chartsImpl.js
+++
@@ -1,6 +0,0 @@
-import { LineChartImpl } from "./LineChartImpl";
-import { PieChartImpl } from "./PieChartImpl";
-import { DoughnutChartImpl } from "./DoughnutChartImpl";
-import { BarChartImpl } from "./BarChartImpl";
-
-export { LineChartImpl, PieChartImpl, DoughnutChartImpl, BarChartImpl };
